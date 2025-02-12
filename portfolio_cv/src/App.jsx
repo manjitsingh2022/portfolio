1c2045d53f4efdb448262001d4a320dff9340e91
manjit singh <ebsmanjitsingh@gmail.com>
2025-02-12 16:25:16 +0530

    add package  gh-pages commit

--- portfolio_cv/src/App.jsx
+++ portfolio_cv/src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
-import About from './components/pages/About'
-import Contact from './components/pages/Contact'
-import Header from './components/pages/Header'
-import Projects from './components/pages/Projects'
-import Skills from './components/pages/Skills'
+// import About from './components/pages/About'
+// import Contact from './components/pages/Contact'
+// import Header from './components/pages/Header'
+// import Projects from './components/pages/Projects'
+// import Skills from './components/pages/Skills'
 
 function App() {
   return (
     <>
-      <Header />
+
+    <p>react jssssss</p>
+      {/* <Header />
       <About />
       <Skills />
       <Projects />
-      <Contact />
+      <Contact /> */}
     </>
   )
 }
--- portfolio_cv/vite.config.js
+++ portfolio_cv/vite.config.js
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Important for Netlify
+  build: {
+    outDir: 'dist', // Ensures correct output folder
+  }
   // base: 'https://github.com/manjitsingh2022/portfolio.git', 
 })
