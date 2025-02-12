9921777e73eb6b5e48f84df30daa1b3985c2a9a1
manjit singh <ebsmanjitsingh@gmail.com>
2025-02-12 17:30:47 +0530

    add file chages

--- portfolio_cv/src/App.jsx
+++ portfolio_cv/src/App.jsx
@@ -1,20 +1,18 @@
 import './App.css'
-// import About from './components/pages/About'
-// import Contact from './components/pages/Contact'
-// import Header from './components/pages/Header'
-// import Projects from './components/pages/Projects'
-// import Skills from './components/pages/Skills'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
+import Header from './components/pages/Header'
+import Projects from './components/pages/Projects'
+import Skills from './components/pages/Skills'
 
 function App() {
   return (
     <>
-
-    <p>react jssssss</p>
-      {/* <Header />
+      <Header />
       <About />
       <Skills />
       <Projects />
-      <Contact /> */}
+      <Contact />
     </>
   )
 }
